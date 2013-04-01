232399436cb52f42146381d9ad39b8fd2f925e29
Irving Greisman <irving.greisman@gmail.com>
2013-04-01 16:30:29 -0700

    Changed order of loading css

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BrigadeFramework::Application.initialize!
+
+#config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+#config.gem 'gdata', :lib => 'gdata'
