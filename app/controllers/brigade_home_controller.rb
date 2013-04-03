487ed9471af5b2c46956b52517d54489fb553dee
Jeff Magee <jeff.d.magee@gmail.com>
2013-04-03 14:32:03 -0700

    created simple brigade theme

--- app/controllers/brigade_home_controller.rb
+++ app/controllers/brigade_home_controller.rb
@@ -3,7 +3,7 @@ class BrigadeHomeController < ApplicationController
   def brigade_home
     @brigade = Brigade.find_by(slug: params[:brigade_slug])
 
-    render :brigade_home, layout: "themes/default"
+    render :brigade_home, layout: "brigades/simple"
   end
 
 end
