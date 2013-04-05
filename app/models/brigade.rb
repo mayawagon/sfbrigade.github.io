bd339014282bc97accaf30d1fb63bed3bc644324
Jeff Magee <jeff.d.magee@gmail.com>
2013-04-05 14:15:44 -0700

    filled out simple theme layout

--- app/models/brigade.rb
+++ app/models/brigade.rb
@@ -8,6 +8,9 @@ class Brigade
 
   field :meetup_url, type: String
   field :github_url, type: String
+  field :google_group_url, type: String
+  field :irc_hashtag, type: String
+  field :irc_url, type: String
   field :twitter_screen_name, type: String
   field :twitter_hashtag, type: String
 
@@ -15,8 +18,14 @@ class Brigade
   field :simple_banner_img_url, type: String
   field :simple_banner_header, type: String
   field :simple_banner_text, type: String
+  field :simple_project_list, type: String
+  field :simple_contact_text, type: String
 
   validates_presence_of :name, :slug
   validates_uniqueness_of :slug
+
+  def simple_projects
+    @_simple_projects ||= simple_project_list.split("\n")
+  end
 
 end
--- spec/models/brigade_spec.rb
+++ spec/models/brigade_spec.rb
@@ -7,11 +7,16 @@ describe Brigade do
     :slug,
     :meetup_url,
     :github_url,
+    :google_group_url,
+    :irc_hashtag,
+    :irc_url,
     :twitter_screen_name,
     :twitter_hashtag,
     :simple_banner_img_url,
     :simple_banner_header,
-    :simple_banner_text
+    :simple_banner_text,
+    :simple_project_list,
+    :simple_contact_text
   ].each do |field|
     it { should allow_mass_assignment_of(:field) }
   end
@@ -20,5 +25,19 @@ describe Brigade do
   it { should validate_presence_of(:slug) }
 
   it { should validate_uniqueness_of(:slug) }
+
+  describe "#simple_projects" do
+    let(:brigade) { Brigade.new(simple_project_list: "1\n2\n3") }
+
+    it "splits the simple project list" do
+      brigade.simple_projects.should == [ "1", "2", "3" ]
+    end
+
+    it "is memoized" do
+      brigade.simple_projects
+      brigade.simple_project_list == ""
+      brigade.simple_projects.should == [ "1", "2", "3" ]
+    end
+  end
 
 end
